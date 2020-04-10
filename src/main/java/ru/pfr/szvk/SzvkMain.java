ccbe064c601202e507075782e3bdfca6ff39cc68
potterdev <gonchar.vladimir@yahoo.com>
2020-04-10 23:07:13 +0300

    It is my first comit at base spring-boot.

---
+++ src/main/java/ru/pfr/szvk/SzvkMain.java
@@ -0,0 +1,11 @@
+package ru.pfr.szvk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzvkMain {
+    public static void main(String[] args) {
+        SpringApplication.run(SzvkMain.class, args);
+    }
+}
