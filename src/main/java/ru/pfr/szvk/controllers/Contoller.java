64414d082cee922428f8c7a7b22d1adfd0d10e5c
092GoncharVV <gonchar.vladimir@hotmail.com>
2020-04-16 14:09:58 +0300

    added securing

--- src/main/java/ru/pfr/szvk/controllers/Contoller.java
+++ src/main/java/ru/pfr/szvk/controllers/Contoller.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class Contoller {
-    @GetMapping("/szvk_spring/")
+    @GetMapping("/")
     public String home( Model model) {
         model.addAttribute("title", "ОПФР по г. Севастополю  СЗВ-К");
         return "home";
     }
 
-    @GetMapping("/szvk_spring/about")
+    @GetMapping("/about")
     public String about( Model model) {
         model.addAttribute("title", "О ПРОЕКТЕ СЗВ-К");
         return "about";
